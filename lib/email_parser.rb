017f42f2bf03bc9f6df0cc0e42ada59c9a26c0f2
github username <github email address>
2021-07-10 16:43:27 +0545

    commit tree 3e3f7841f97d68bf350d4d4f1c1bb5a763eb80c3

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,14 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+    attr_accessor :parser
+    def initialize(parser)
+        @parser = parser
+    end
+
+    def parse
+       @parser.split(/[\s,]+/).uniq
+    end
+end
